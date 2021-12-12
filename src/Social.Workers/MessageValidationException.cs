b6372861dbafb56b19a91ca5cb5d3e9f818dd477
Gary Strasdin <garystrasdin@gmail.com>
2021-12-12 00:04:05 -0600

    Exception for message validation failures

---
+++ src/Social.Workers/MessageValidationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Dataflow;
+
+namespace Social.Workers
+{
+    internal class MessageValidationException : ApplicationException
+    {
+        public MessageValidationException(string message, IMessage queueMessage, List<ValidationResult> validationResults) : base(message)
+        {
+            QueueMessage = queueMessage;
+            ValidationResults = validationResults;
+        }
+
+        public IMessage QueueMessage { get; }
+        public List<ValidationResult> ValidationResults { get; set; }
+    }
+}
