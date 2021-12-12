761540ecf4ac935122b3404e8a6b1e88affb47ef
Gary Strasdin <garystrasdin@gmail.com>
2021-12-12 00:12:47 -0600

    Added social media and twitter controllers

---
+++ src/Social.Api/Controllers/TwitterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace Social.Api.Controllers
+{
+    [ApiController]
+    [Route("twitter")]
+    public class TwitterController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public TwitterController(ILogger logger)
+        {
+            _logger = logger;
+        }
+    }
+}
