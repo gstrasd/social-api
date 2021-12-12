6c54fd2bd2caf86d95de60b9c04e9578cefab9d3
Gary Strasdin <garystrasdin@gmail.com>
2021-12-12 00:03:35 -0600

    Added discover-twitter-account message validation

--- src/Social.Messages/DiscoverTwitterAccountMessage.cs
+++ src/Social.Messages/DiscoverTwitterAccountMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -10,10 +11,16 @@ namespace Social.Messages
 {
     public class DiscoverTwitterAccountMessage : IMessage
     {
+        [Required]
         [JsonPropertyName("correlationId")]
         public Guid CorrelationId { get; set; }
+        
+        [Required]
         [JsonPropertyName("providerId")]
         public string ProviderId { get; set; }
+        
+        [Required]
+        [RegularExpression("^[a-zA-Z\\d_]{4, 15}$", ErrorMessage = "Twitter username must match expression ^[a-zA-Z\\d_]{4, 15}$")]
         [JsonPropertyName("twitterUsername")]
         public string TwitterUsername { get; set; }
     }
